550154ab997de34a7048099131d76808fa13eba8
trite2k3 <32780035+trite2k3@users.noreply.github.com>
2023-10-29 23:50:23 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+SRed=255
+SGreen=255
+SBlue=255
+ColorDelta=0
+CDelta=10
+SX=0
+SY=0
+EX=0
+EY=0
+
 #Run the binary
-pixelsearch=$(./search 255 255 255 0 10 0 0 0 0)
+pixelsearch=$(./search $SRed $SGreen $SBlue $ColorDelta $CDelta $SX $SY $EX $EY)
 
 #split the output into two variables
 IFS=',' read -ra values <<< "$(echo "$pixelsearch" | awk -F',' '{print $1,$2}')"
