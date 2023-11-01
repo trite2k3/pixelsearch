7424f4163d03bfe3a60fcc72cd97a12defa312ff
trite2k3 <32780035+trite2k3@users.noreply.github.com>
2023-11-01 14:25:40 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -84,7 +84,7 @@ function joinbg
     sleep 3
     #join battle
     echo "pressing join button"
-    xdotool mousemove --sync 2900 890
+    xdotool mousemove --sync 2901 887
     xdotool click 1
     sleep 1
 }
@@ -97,17 +97,15 @@ function enterbattle
     xdotool click 1
 }
 
-#use sleep 3; xdotool getmouselocation to find mousepos
-#SRed=0
-#SGreen=0
-#SBlue=0
-#ColorDelta=0w
-#CDelta=10
-#SX=4991
-#SY=77
-#EX=1
-#EY=1
+function exitbattle
+{
+    #trying to join bq pop
+    xdotool mousemove --sync 3845 1001
+    sleep 1
+    xdotool click 1
+}
 
+#use sleep 3; xdotool getmouselocation to find mousepos
 SRed=0
 SGreen=0
 SBlue=0
@@ -120,20 +118,28 @@ EY=1
 
 while true
 do
+    SX=4960
+    SY=63
     if limitedsearch "$SRed, $SGreen, $SBlue, $ColorDelta, $CDelta, $SX, $SY, $EX, $EY"
     then
         #move mouse to pos
         #echo "Moving mouse to "$varX$varY
         #xdotool mousemove $varX $varY
         echo "We are in Stormwind Keep."
-        while sleep 20
+
+        counter=0
+
+        while [ $counter -lt 2 ]
         do
-        joinbg
-        sleep 1
-        enterbattle
-        sleep 25
-        echo "anti afk"
-        xdotool key f
+            sleep 2
+            joinbg
+            sleep 1
+            enterbattle
+            sleep 20
+            echo "anti afk"
+            xdotool key f
+            sleep 13
+            ((counter++))
         done
 
     else
@@ -142,20 +148,29 @@ do
         sleep 3
         xdotool keyup Up
         xdotool keydown Right
-        sleep 0.21
+        sleep 0.245
         xdotool keyup Right
+        xdotool keydown d
+        sleep 1
+        xdotool keyup d
+        sleep 0.1
         xdotool keydown Up
         sleep 7.5
         xdotool keyup Up
-        echo "waiting for gates to open"
-        sleep 60
+        sleep 0.1
+        #xdotool keydown d
+        #sleep 1
+        #xdotool keyup d
+        #echo "waiting for gates to open"
+        #sleep 60
+        echo "walking"
         xdotool keydown Up
         sleep 30
         xdotool keyup Up
 
         counter=0
 
-        while [ $counter -lt 10 ]
+        while [ $counter -lt 2 ]
         do
             xdotool key Down
             sleep 0.1
@@ -172,6 +187,18 @@ do
             xdotool key g
             echo "sneaky afk"
             sleep 30
+
+            SX=4244
+            SY=1001
+            if limitedsearch "$SRed, $SGreen, $SBlue, $ColorDelta, $CDelta, $SX, $SY, $EX, $EY"
+            then
+                echo "exiting battle"
+                exitbattle
+                continue
+            else
+                echo "bg not finished"
+            fi
+
             ((counter++))
         done
     fi
